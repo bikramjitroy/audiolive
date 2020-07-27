4333e885ffe799c9835c8e6bde859d527925b706
Bikramjit Roy <bikramjitroy@drishti-soft.com>
2020-07-27 22:00:33 +0530

    Support for merging

--- audiolive.js
+++ audiolive.js
@@ -1,15 +1,15 @@
-const https = require('https')
+const https = require('https');
 var express = require("express");
 const bodyParser = require('body-parser');
 const util = require('util');
 const exec = util.promisify(require('child_process').exec);
 
-const multer  = require('multer') //use multer to upload blob data
+const multer  = require('multer'); //use multer to upload blob data
 const upload = multer(); // set multer to be the upload variable (just like express, see above ( include it, then use it/set it up))
 const fs = require('fs');
 
 
-const APP_PORT = 2402
+const APP_PORT = 2402;
 
 
 var app = express();
@@ -17,27 +17,27 @@ var app = express();
 app.use(express.static('public'));
 app.use('/audio', express.static(__dirname + '/public/audio'));
 //app.use(bodyParser.urlencoded({ extended: true }));
-var urlencodedParser = bodyParser.urlencoded({ extended: false })
-var jsonParser = bodyParser.json()
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+var jsonParser = bodyParser.json();
 
 
 app.post('/uploadfile', upload.single('soundBlob'), function (req, res, next) {
   console.log(req.body.duration, req.body.startTime); // see what got uploaded
   
 
-  let basePath = 'public/uploads/'
-  let uploadLocation = __dirname + '/' + basePath + req.file.originalname + ".webm" // where to save the file to. make sure the incoming name has a .wav extension
-  console.log("ST", req.file.startTime, req.endTime)
+  let basePath = 'public/uploads/';
+  let uploadLocation = __dirname + '/' + basePath + req.file.originalname + ".webm"; // where to save the file to. make sure the incoming name has a .wav extension
+  console.log("ST", req.file.startTime, req.endTime);
 
   //fs.writeFileSync(uploadLocation, Buffer.from(new Uint8Array(req.file.buffer)),{flag:'w'}); // write the blob to the server as a file
   fs.writeFileSync(uploadLocation, req.file.buffer,{flag:'w'}); // write the blob to the server as a file
 
-  let filePath = basePath + req.file.originalname
-  let convertCommand = 'ffmpeg -y -i ' + filePath + '.webm -ar 8000 -acodec pcm_s16le ' + filePath + '.wav'
+  let filePath = basePath + req.file.originalname;
+  let convertCommand = 'ffmpeg -y -i ' + filePath + '.webm -ar 8000 -acodec pcm_s16le ' + filePath + '.wav';
 
-  console.log("ConvertCommand:",convertCommand)
+  console.log("ConvertCommand:",convertCommand);
 
-  const exec = require('child_process').exec
+  const exec = require('child_process').exec;
   exec(convertCommand, (err, stdout, stderr) => {
       res.send("uploads/" + req.file.originalname + ".wav");
       //let trimCommand = 'sox ' + filePath + '.wav ' + filePath + '-trim.wav trim ' + req.body.startTime + ' ' + req.body.duration
@@ -45,23 +45,23 @@ app.post('/uploadfile', upload.single('soundBlob'), function (req, res, next) {
       ////process.stdout.write(stdout);
       //    res.send("uploads/" + req.file.originalname + "-trim.wav"); //send back that everything went ok
       //})	      
-  })
+  });
 
 });
 
 app.post('/process', urlencodedParser,function (req, res) {
   console.log('Got from data process:', req.body);
   
-  let dstFile = req.body.userName
-  let dstFileTrim = 'uploads/currentFile-' + dstFile + '-trim.wav'
+  let dstFile = req.body.userName;
+  let dstFileTrim = 'uploads/currentFile-' + dstFile + '-trim.wav';
 
-  let command = 'sox public/' + req.body.srcAudioName + ' public/' + dstFileTrim + ' trim ' +  req.body.startTime + ' ' + req.body.duration
-  console.log("Command:",command)
+  let command = 'sox public/' + req.body.srcAudioName + ' public/' + dstFileTrim + ' trim ' +  req.body.startTime + ' ' + req.body.duration;
+  console.log("Command:",command);
   exec(command).then(function(resp) {
-	 console.log("Response:",dstFileTrim)
-	 res.send(dstFileTrim)
+	 console.log("Response:",dstFileTrim);
+	 res.send(dstFileTrim);
   });
-})
+});
 
 app.post('/uploadPrompt', urlencodedParser, function (req, res) {
   console.log('Got from data uploadPrompt:', req.body);
@@ -71,13 +71,13 @@ app.post('/uploadPrompt', urlencodedParser, function (req, res) {
       fs.mkdirSync(onlyPath, {recursive: true});
   }
   
-  let command = 'cp public/' + req.body.fromFile + ' public/' + req.body.fullPath 
-  console.log("Command:",command)
+  let command = 'cp public/' + req.body.fromFile + ' public/' + req.body.fullPath;
+  console.log("Command:",command);
   exec(command).then(function(resp) {
-         console.log("Response:", resp)
-         res.send(req.body.fullPath)
+         console.log("Response:", resp);
+         res.send(req.body.fullPath);
   });
-})
+});
 
 
 
@@ -85,31 +85,31 @@ app.post('/uploadPrompt', urlencodedParser, function (req, res) {
 app.post('/dates', jsonParser, function (req, res) {
   console.log('Got from data- dates:', req.body);
   
-  let language = req.body.language
-  let user = req.body.user
-  let folder = '/date/'
-  let filename = 'metadata/' + language + "-date.json"
+  let language = req.body.language;
+  let user = req.body.user;
+  let folder = '/date/';
+  let filename = 'metadata/' + language + "-date.json";
 
   let rawdata = fs.readFileSync(filename);
   let rawJson = JSON.parse(rawdata);
   let updatedJson = JSON.parse(rawdata);
 
   for (var promptKey in rawJson){
-      let path = 'public/recordings/' + user + '/' + language + folder + rawJson[promptKey]['filename']
+      let path = 'public/recordings/' + user + '/' + language + folder + rawJson[promptKey]['filename'];
 
-      updatedJson[promptKey]['fileExist'] = false
+      updatedJson[promptKey]['fileExist'] = false;
       try {
           if (fs.existsSync(path)) {
-              updatedJson[promptKey]['fileExist'] = true
+              updatedJson[promptKey]['fileExist'] = true;
           }
       } catch(err) {
-          console.error(err)
+          console.error(err);
       }
   }
 
-  console.log(updatedJson)
-  res.send(updatedJson)
-})
+  console.log(updatedJson);
+  res.send(updatedJson);
+});
 
 app.post('/numbers', jsonParser, function (req, res) {
   console.log('Got from data- numbers:', req.body);
--- tts-recording.js
+++ tts-recording.js
@@ -1,76 +1,245 @@
 var DOMParser = require('dom-parser');
-var converter = require('number-to-words');
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
+const fs = require('fs');
+var md5 = require('md5');
 
-function numberVsAudioFile(number) {
-   console.log("Number To Word:",converted)
 
-   let concat = ''
+async function mergeFiles(audioFiles, outfile) {
+
+    let command = "sox";
+    for (let i=0; i < audioFiles.length; i++ ) {
+        command = command + " " +audioFiles[i];
+    }
+    command = command + " " + outfile;
+
+    console.log("Command:",command);
+    return exec(command).then(function(resp) {
+       console.log("Converted:",outfile);
+       return outfile;
+    });
+}
+
+function numberToText(numberToText, numberJson) {
+    let number = numberToText;
+
+   let concat = '';
    if (number === 0) {
-       console.log("RECORD -- ZERO")       
+       console.log("ERROR:----RECORD -- ZERO");     
    }
 
-   let lakh = parseInt(number/100000)
+   let lakh = parseInt(number/100000);
    //console.log(lakh, " Lakh")
    if (lakh> 0) {
-      concat = String(lakh) + " lakh "
+      concat = String(lakh) + " lakh ";
    }
-   number = number%100000 
+   number = number%100000;
 
-   let thousand = parseInt(number/1000)
+   let thousand = parseInt(number/1000);
    //console.log(thousand, " thousand")
    if (thousand> 0) {
-       concat = concat + String(thousand) + " thousand "
+       concat = concat + String(thousand) + " thousand ";
    }
-   number = number%1000
+   number = number%1000;
 
-   let hundred = parseInt(number/100)
+   let hundred = parseInt(number/100);
    //console.log(hundred, " hundred ")
    if (hundred> 0) {
-       concat = concat + String(hundred) + "-hundred "
+       concat = concat + String(hundred) + "-hundred ";
    }
-   number = number%100
+   number = number%100;
 
    //console.log(number)
    if (number> 0) {
-       concat = concat + String(number)
+       concat = concat + String(number);
    }
 
-   console.log(concat)
+   console.log("TokenText: ",numberToText, concat);
+   return concat;
 }
 
-function processNumbers(allNumbers) {
-    audioSegments = []
-    for (let i=0; i < allNumbers.length; i++) {
-         let number = parseInt(allNumbers[i].innerHTML);
-         numberVsAudioFile(number) 
+function getSSMLTokens(textForTTS) {
+        
+    //Collect Tokens
+    let splitTokenFromInput = [];
+    let tokenStartIndex = 0;
+    let tokenEndIndex = 0;
+    let tokenState = 0;
+
+    let tokenOutsideStart = 0;
+    let tokenOutsideEnd = 0;
+
+    for (let i = 0; i < textForTTS.length; i++) {
+
+        if (textForTTS[i]=== '<' && tokenState===0) {
+            tokenStartIndex = i;
+            tokenState = tokenState + 1;
+
+            //This is the finish of outside token
+            tokenOutsideEnd = i;
+            let text = textForTTS.substring(tokenOutsideStart, tokenOutsideEnd);
+
+            console.log(5,"TOKEN-Out:",text);
+
+            if (text === " " || text ==="") {
+                //Don't add empty tokens
+            } else {
+                splitTokenFromInput.push(text.trim());
+            }
+        }
+
+        if (textForTTS[i]=== '>') {
+            if (tokenState === 1) {
+                tokenState = tokenState + 1;
+                continue;
+            } else {
+                tokenState = 0;
+            }
+            tokenEndIndex = i;
+            let text = textForTTS.substring(tokenStartIndex, tokenEndIndex+1);
+            console.log(7,"TOKEN:",text);
+            splitTokenFromInput.push(text.trim());
+
+            //This is the new start of Outside token
+            tokenOutsideStart = i + 1;
+        }
     }
+
+    if (textForTTS.length - tokenOutsideStart > 0) {
+        let text = textForTTS.substring(tokenOutsideStart, textForTTS.length-1);
+        console.log(textForTTS.length, tokenOutsideStart, "TOKEN-Out:",text.trim());
+        if (text === " " || text ==="") {
+            //Don't add empty tokens
+        } else {
+            splitTokenFromInput.push(text);
+        }
+    }
+
+    return splitTokenFromInput;
 }
 
 
+function removeTag(tagStr, start, end) {
+    return tagStr.replace(start,"").replace(end,"");
+}
 
+function audioFilesFromTokens(convertedText, voiceProfile, languageCode, dataFolder, dataJSON) {
+    let tokens = convertedText.split(" ");
+    let audioTokens = [];
+    //Search by key
+    for (let i = 0; i < tokens.length; i++) {
+        let fullPath = "public/recordings/" + voiceProfile + "/" + languageCode + "/" + dataFolder + "/" + dataJSON[tokens[i]].filename;
+        audioTokens.push(fullPath);
+    }
 
-function responseFile(ssmlText, languageCode, voiceProfile, botId, sessionId) {
+    return audioTokens;
+}
+
+function audioFileFromText(token, voiceProfile, languageCode, dataFolder, dataJSON) {
+
+    //Search by text
+    for (var key of Object.keys(dataJSON)) {
+        if (token == dataJSON[key].text) {
+            let fullPath = "public/recordings/" + voiceProfile + "/" + languageCode + "/" + dataFolder + "/" + dataJSON[key].filename;
+            return fullPath;
+        }
+    }
+
+    return;
+}
+
+async function responseFile(ssmlText, languageCode, voiceProfile, botId, sessionId) {
+
     var oParser = new DOMParser();
     var oDOM = oParser.parseFromString(ssmlText);
-    // print the name of the root element or error message
-    //console.log(oDOM.documentElement.speech == "parsererror" ? "error while parsing" : oDOM.documentElement.speech); 
+
     console.log(oDOM.getElementsByTagName('speech')[0].innerHTML); 
-    let textForTTS = oDOM.getElementsByTagName('speech')[0].innerHTML
+    let textForTTS = oDOM.getElementsByTagName('speech')[0].innerHTML;
 
-    
-    console.log(oDOM.getElementsByTagName("number")[0].innerHTML); 
+    //NUMBER
+    let numberJsonFile = "metadata/" + languageCode + "-numbers.json"; 
+    let numberJson = JSON.parse(fs.readFileSync(numberJsonFile));
 
-    let allNumbers = oDOM.getElementsByTagName("number")
-    processNumbers(allNumbers) 
+    //DATE
+    let dateJsonFile = "metadata/" + languageCode + "-date.json"; 
+    let dateJson = JSON.parse(fs.readFileSync(dateJsonFile));
 
+    //NAME
+    let nameJsonFile = "metadata/" + languageCode + "-names.json"; 
+    let nameJson = JSON.parse(fs.readFileSync(nameJsonFile));
 
+    //BOT-JSON
+    let botJsonFile = "metadata/" + languageCode + "-botid-" + botId + ".json"; 
+    let botJson = JSON.parse(fs.readFileSync(botJsonFile));
 
-    console.log(oDOM.getElementsByTagName("date")[0].innerHTML); 
+    let tokens = getSSMLTokens(textForTTS);
+    console.log("TOKENS:-------", tokens);
+
+    let fileConcatPromises = [];
+    let tokenFiles = [];
+
+    for (let i =0; i < tokens.length; i++) {
+        let token = tokens[i];
+        
+        if (token.startsWith("<number>")) {
+            let textWithoutTag = removeTag(token,"<number>","</number>");
+
+            let numberText = parseInt(textWithoutTag);
+            let convertedText = numberToText(numberText, numberJson);
+
+            let outFileName = convertedText.split(" ").join('--') + ".wav";
+            let outfile = "tts_final/concat_number/" + voiceProfile + "-" + languageCode + "-" + outFileName;
+
+            //Get full path of token audio files to make it a number
+            let audioFiles = audioFilesFromTokens(convertedText, voiceProfile, languageCode, "numbers", numberJson);
+            fileConcatPromises.push(mergeFiles(audioFiles, outfile));
+            tokenFiles.push(outfile)
+
+        } else if (token.startsWith("<date>")) {
+            let textWithoutTag = removeTag(token,"<date>","</date>");
+            
+            let outFileName = textWithoutTag.split(" ").join('--') + ".wav";
+            let outfile = "tts_final/concat_date/" + voiceProfile + "-" + languageCode + "-" + outFileName;
+
+            //Get full path of token audio files to make it a date
+            let audioFiles = audioFilesFromTokens(textWithoutTag, voiceProfile, languageCode, "date", dateJson);
+            fileConcatPromises.push(mergeFiles(audioFiles, outfile));
+            tokenFiles.push(outfile)
+
+        } else if (token.startsWith("<name>")) {
+            let textWithoutTag = removeTag(token,"<name>","</name>");
+            let outfile = audioFileFromText(textWithoutTag, voiceProfile, languageCode, "names", nameJson);
+
+            tokenFiles.push(outfile);
+        } else {
+            //console.log(1,"OPEN-Token", token);
+            let outfile = audioFileFromText(token, voiceProfile, languageCode, botId, botJson);
+            //console.log(11,"OPEN-Token-OUT", outfile);
+            
+            tokenFiles.push(outfile);
+        }
+    }
+
+    let finalOut = "tts_final/concat_final/" + voiceProfile + "-" + languageCode + "-" + botId + "-" + md5(textForTTS) + ".wav";
+
+    return Promise.all(fileConcatPromises).then((values) => {
+        console.log("Promise Return-", values);
+
+        return mergeFiles(tokenFiles, finalOut).then(function(finalOutFilename) {
+            console.log("TokenFilenames:", tokenFiles, finalOut);
+            return finalOut;
+        });
+    });
 }
 
-let ssmlText = '<speech>Today we will spech <number>23596</number> <number>6</number> <number>96</number> <number>596</number> <number>3596</number> rupees on <date>29 July</date></speech>';
-let languageCode = "en"
-let voiceProfile = "sonam"
-let botId = "insurenceSales" 
-let sessionId = "merge1-"
-responseFile(ssmlText, languageCode, voiceProfile, botId, sessionId)
+//var converter = require('number-to-words');
+
+let ssmlText = '<speech>Today we will speak <number>23596</number> <number>6</number> <number>96</number> <number>596</number> <number>3596</number> rupees on <date>29 Jul</date> </speech>';
+let languageCode = "en";
+let voiceProfile = "sonam";
+let botId = "insurenceSales"; 
+let sessionId = "merge1-";
+
+responseFile(ssmlText, languageCode, voiceProfile, botId, sessionId).then((outvalue) =>{
+    console.log("OUTVALUE", outvalue);
+})
